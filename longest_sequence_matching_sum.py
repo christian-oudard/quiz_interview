4d63ef0b41056a04317a9b9d99be8bbbc1a0d016
Christian Oudard <christian.oudard@gmail.com>
2022-02-09 14:01:16 -0800

    Clean up longest_sequence_matching_sum.

--- longest_sequence_matching_sum.py
+++ longest_sequence_matching_sum.py
@@ -1,17 +1,25 @@
 """
-Given an array A and integer k, find the length of the longest sub-array (consecutive) whose sum is k.
+Given an array of integers A, and a number K, find the longest consecutive
+subsequence which sums to K. Return the start and end indices of the result.
+If no such range exists, return None.
 
-Example: given A = [100,1,2,3,-1,0,0,5,6,7], k = 5, return 6 (which is the length of [1,2,3,-1,0,0])
+Example:
+Given A = [100,1,2,3,-1,0,0,5,6,7], K = 5, the longest result is [1,2,3,-1,0,0],
+which has length 6, starting at index 1 and ending at 7.
 
->>> k = 5
 >>> arr = [100, 1, 2, 3, -1, 0, 0, 5, 6, 7]
->>> max_seq_length(arr, k)
-6
+>>> lo, hi = largest_range_matching_sum(arr, 5)
+>>> lo, hi
+(1, 7)
+>>> sum(arr[lo:hi])
+5
+
+>>> assert largest_range_matching_sum([1, 2, 3, 4, 5], 1000) is None
 """
 
 from collections import defaultdict
 
-def max_seq_length(arr, k):
+def largest_range_matching_sum(arr, k):
     # Find the partial sums of every sub-array starting from the far left, in O(n) time.
     left_sum = 0
     left_sums = [0]
@@ -26,6 +34,7 @@ def max_seq_length(arr, k):
 
     # Find a difference between left sums that equals the target number.
     max_length = 0
+    best_start_index = None
     for i, s in enumerate(left_sums):
         positions = left_sums_table[s + k]
         if len(positions) == 0:
@@ -33,5 +42,9 @@ def max_seq_length(arr, k):
         seq_length = max(positions) - i
         if seq_length > max_length:
             max_length = seq_length
+            best_start_index = i
 
-    return max_length
+    if best_start_index is None:
+        return None
+
+    return (best_start_index, best_start_index + max_length)
